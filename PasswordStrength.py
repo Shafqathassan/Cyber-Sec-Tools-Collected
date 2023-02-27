67f623b2f3275fff11bb57a2495d8c55e36f78d5
Shafqathassan <93049662+Shafqathassan@users.noreply.github.com>
2023-02-27 06:09:56 +0000

    Add files via upload
    This program is written in python and while you run it you have to input your password and it will depict your password strength and compare it from file having common passwords. 
    Do remember the password file should be in same folder as of program.
    Would love to hear feedback !
    With regards,
    Shafqat .

---
+++ PasswordStrength.py
@@ -0,0 +1,63 @@
+#Author : Shafqat hassan
+#Dated : 27th; of feb 2023
+#Repo : Shafqathassan/Cyber-Sec-Tools
+
+import string
+
+
+password = input("Enter the password: ")
+
+
+upper_case = any([1 if c in string.ascii_uppercase else 0 for c in password])
+lower_case = any([1 if c in string.ascii_lowercase else 0 for c in password])
+special = any([1 if c in string.ascii_punctuation else 0 for c in password])
+digits = any([1 if c in string.ascii_digits else 0 for c in password])
+
+
+characters = [upper_case, lower_case, special, digits]
+
+
+length =len(password)
+
+score = 0
+
+with open('common.txt', 'r') as f:
+    common = f.read().splitlines()
+
+if password in common:
+    print("password was found in common list . score : 0 / 7")
+    exit()
+
+if length > 8:
+    score += 1
+if length > 12:
+    score += 1
+if length > 17:
+    score += 1
+if length > 20:
+    score += 1
+
+
+print(f"Password length is {str(length)}, adding { str(score)} points!")
+
+if sum(characters) > 1:
+    score += 1
+if sum(characters) > 2:
+    score += 1
+if sum(characters) > 3:
+    score += 1
+print (f"Password has {str(sum(characters))} different character types, adding {str(sum(characters) -1)} points !")
+
+if score < 4:
+    print (f"password is quite weak! Score: {str(score)} / 7")
+elif score == 4:
+    print(f"Password is ok ! Score: {str(score)} / 7")
+elif score > 4 and score < 6 :
+    print(f"Password is pretty good!  Score: {str(score)} / 7")
+elif score > 6:
+    print(f"Password is stro! Score: {str(score)} / 7")
+
+
+
+
+print(upper_case)
