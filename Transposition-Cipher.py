801de9c1833c5cf2c78c0b3600f315dd0dc8a14d
Shafqathassan <93049662+Shafqathassan@users.noreply.github.com>
2023-03-18 09:58:49 +0000

    Update Transposition-Cipher.py

--- Transposition-Cipher.py
+++ Transposition-Cipher.py
@@ -13,6 +13,7 @@ ciphertext = ''
 for index in sorted(order.keys()):
    for part in split_len(plaintext, len(key)):
       try:ciphertext += part[order[index]]
+         #The IndexError exception means that you are trying to access an index that doesn't exist.
          except IndexError:
             continue
    return ciphertext
