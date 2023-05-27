a35ca9ac76239e4457f2184b8478d317e2586943
Shafqat <93049662+Shafqathassan@users.noreply.github.com>
2023-05-27 10:04:58 +0530

    Add files via upload

---
+++ TCPClient.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import socket
+
+clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#specify host
+#host = '192.168.1.104'
+host = socket.gethostbyname()
+
+port = 444
+
+clientsocket.connect(('192.168.1.104', port))
+
+message = clientsocket.recv(1024)
+
+clientsocket.close()
+
+print(message.decode('ascii'))
+
