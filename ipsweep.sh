ba5998250ac0092668ce040057ce3ccc547cfa53
Shafqat <93049662+Shafqathassan@users.noreply.github.com>
2023-06-11 11:05:47 +0530

    Update ipsweep.sh

--- ipsweep.sh
+++ ipsweep.sh
@@ -1,7 +1,7 @@
 #Author : ShafqatHassan
 #Dated : 1st; of Mar 2023
 #Repo : Shafqathassan/Cyber-Sec_Tools
-#Credit : Georgia Weidman and HeathAdams 
+#Credit : Georgia Weidman and HeathAdams https://youtube.com/@TCMSecurityAcademy
 
 #!/usr/bin/bash is a shebang line used in script files to set bash, present in the '/bin' directory.It defines an absolute path /usr/bin/bash to the Bash shell 
 #(for more info : https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjBzO_tsJ3-AhUvplYBHVQjCAwQFnoECA8QAQ&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FShebang_(Unix)&usg=AOvVaw2JO5JC-ojjNYqhPOD6J7aP )
